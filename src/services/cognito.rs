fcdcf6deacd93c9a1d2afb2544badcad78f1c198
smtamnay <smtamay@ixmatix.com>
2025-06-13 17:01:31 -0600

    ✨ refactor: update init_cognito_user_manager to use UserManager instead of CognitoUserManager

--- src/services/cognito.rs
+++ src/services/cognito.rs
@@ -1,8 +1,8 @@
-use crate::domains::auth::dynamo::CognitoUserManager;
+use crate::domains::auth::user_manager::UserManager;
 use axum::http::StatusCode;
 
-pub async fn init_cognito_user_manager() -> Result<CognitoUserManager, (StatusCode, String)> {
-    CognitoUserManager::new()
+pub async fn init_cognito_user_manager() -> Result<UserManager, (StatusCode, String)> {
+    UserManager::new()
         .await
         .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
 }
